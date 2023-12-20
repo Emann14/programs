08549bdf04fe596f9a1fa59db0f7f1dd2424af22
Emann14 <154388818+Emann14@users.noreply.github.com>
2023-12-19 18:58:23 -0600

    Add files via upload

---
+++ program1/src/program1.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : program1.cpp
+// Author      : Evan M
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// TODO Auto-generated constructor stub
+		string firstName;
+		string lastName;
+		string address;
+		string city;
+		string zipcode;
+
+		firstName = "John";
+		lastName = "Doe";
+		address = "8132 Heron Dr";
+		city = "Fort Worth";
+		zipcode = "76248";
+
+		cout << firstName << " " << lastName << " " << address << " " << city << " " << zipcode;
+		return 0;
+}
---
+++ program2/src/program2.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : program2.cpp
+// Author      : Evan M
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<iostream>
+#include<conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main()
+{
+
+     //Standard Ouput Statement
+     cout<<"Welcome to this C++ Program"<<endl;
+
+     cout<<"I have corrected all errors for this program." <<endl;
+
+     // Wait For Output Screen
+
+     //Main Function return Statement
+
+}
